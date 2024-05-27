d2b4f3b603e6fcdc3bf4190818b6543bd7cd1a01
Cez <cezembre.rithie@e-artsup.net>
2024-05-27 10:14:33 +0200

    le mauvais

--- Kit cat/Assets/Script/Game/Player/Player_mouvement.cs
+++ Kit cat/Assets/Script/Game/Player/Player_mouvement.cs
@@ -1,82 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Animations;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 
-public class Player_mouvement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+public class PlayerController : MonoBehaviour
 {
-    //Classe public
-    public Animator anim;
-    public float movingSpeed = 5;
-    public int jumpPower = 5;
-    public Transform groundCheck;
-    public LayerMask groundLayer;
-    public Transform Backup;
-    public Door door;
-    public GameObject objectAttrapper;
+    // Move player in 2D space
+    public float maxSpeed = 3.4f;
+    public float jumpHeight = 6.5f;
+    public float gravityScale = 1.5f;
+
+    // Animation and controls parameters
+    bool facingRight = true;
+    float moveDirection = 0;
+    bool isGrounded = false;
+    [SerializeField] Door door;
+    [SerializeField] private float rayDistance;
+
+    // Components
+    Transform t;
+    Rigidbody2D r2d;
+    CapsuleCollider2D mainCollider;
+    private Animator animController;
     [SerializeField] Transform PlayerGrabPosition;
-    [SerializeField] float rayDistance;
-    //Classe private
-    private Rigidbody2D rb;
-    //Fonctions
-    Vector2 CheckpointPos = new(5, 5);
-    bool isGrounded;
-
-
-
-
+    public GameObject objectAttrapper;
+    public Transform Backup;
+    [SerializeField] Vector2 CheckpointPos = new(5, 5);
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        CheckpointPos = transform.position;
-        //door = GameObject.FindGameObjectWithTag("Door").GetComponent<Door>();
+        t = GetComponent<Transform>();
+        r2d = GetComponent<Rigidbody2D>();
+        mainCollider = GetComponent<CapsuleCollider2D>();
+        animController = GetComponent<Animator>();
+
+        r2d.freezeRotation = true;
+        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        r2d.gravityScale = gravityScale;
+        facingRight = t.localScale.x > 0;
     }
 
-
-//Regarde si le joueur rentre en collision avec une zone de Respawn
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Respawn"))
-        {
-            Reset();
-        }
-
-        if (collision.CompareTag("Key"))
-        {
-            door.Open();
-        }
-
-        if (collision.CompareTag("Checkpoint"))
-        {
-            UpdateCheckpoint(transform.position);
-        }
-
-       /* if (collision.CompareTag("Teleporter"))
-        {
-            Teleport();
-        }*/
-    }
-
-// Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.5f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
-     /*   if (Input.GetButtonDown("Jump") && isGrounded)
+
+        // Movement controls
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || Mathf.Abs(r2d.velocity.x) > 0.01f))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-        } */
+            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
+        }
+        else
+        {
+            if (isGrounded || r2d.velocity.magnitude < 0.01f)
+            {
+                moveDirection = 0;
+            }
+        }
 
-        float horizontalMouvement = Input.GetAxis("Horizontal") * movingSpeed;
+        // Change facing direction
+        if (moveDirection != 0)
+        {
+            animController.SetBool("iswalking", true);
+            if (moveDirection > 0 && !facingRight)
+            {
+                facingRight = true;
+                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, transform.localScale.z);
+            }
 
-        if (Mathf.Abs(horizontalMouvement) > 0) { anim.SetBool("iswalking", true); Debug.Log("move"); }
-        else { anim.SetBool("iswalking", false); }
-        
+            if (moveDirection < 0 && facingRight)
+            {
+                facingRight = false;
+                t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
+            }
+        }
+        else
+            animController.SetBool("iswalking", false);
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
+            animController.SetTrigger("Jump");
+        }
 
         RaycastHit2D hitInfo = Physics2D.Raycast(PlayerGrabPosition.position, transform.right, rayDistance);
 
@@ -88,8 +94,11 @@ public class Player_mouvement : MonoBehaviour
                 print("attraper");
                 objectAttrapper = hitInfo.collider.gameObject;
                 objectAttrapper.GetComponent<Rigidbody2D>().isKinematic = true;
-                objectAttrapper.transform.SetParent(transform); //definir une velocite et la mettre a jour a chaque update, évite le traversage de mur, la vélocité doit etre identique a celle du joueur
-                objectAttrapper.transform.localPosition = new Vector2(Backup.localPosition.x * objectAttrapper.transform.lossyScale.x, objectAttrapper.transform.localPosition.y);
+                objectAttrapper.transform
+                    .SetParent(transform); //definir une velocite et la mettre a jour a chaque update, ?vite le traversage de mur, la v?locit? doit etre identique a celle du joueur
+                objectAttrapper.transform.localPosition = new Vector2(
+                    Backup.localPosition.x * objectAttrapper.transform.lossyScale.x,
+                    objectAttrapper.transform.localPosition.y);
 
             }
 
@@ -101,42 +110,52 @@ public class Player_mouvement : MonoBehaviour
                 objectAttrapper = null;
             }
         }
-        MovePlayer(horizontalMouvement);
-    }
-    public void UpdateCheckpoint(Vector2 Pos)
-    {
-        CheckpointPos = new Vector2(Pos.x, Pos.y);
-        Debug.Log(CheckpointPos);
     }
 
-    void MovePlayer(float _horizontalMovement)
+    void FixedUpdate()
     {
-        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
-        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref targetVelocity, 0f);
-        FlipCharacter();
-    }
-    public void FlipCharacter()
-    {
-        if (objectAttrapper == null)
+        Bounds colliderBounds = mainCollider.bounds;
+        float colliderRadius = mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
+        Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
+        // Check if player is grounded
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderRadius);
+        //Check if any of the overlapping colliders are not player collider, if so, set isGrounded to true
+        isGrounded = false;
+        if (colliders.Length > 0)
         {
-            if (rb.velocity.x < 0)
+            for (int i = 0; i < colliders.Length; i++)
             {
-                transform.localRotation = Quaternion.Euler(0, 180, 0);
-            }
-            else if (rb.velocity.x > 0)
-            {
-                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                if (colliders[i] != mainCollider)
+                {
+                    isGrounded = true;
+                    break;
+                }
             }
         }
+
+        // Apply movement velocity
+        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
+
+        // Simple debug
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), isGrounded ? Color.green : Color.red);
     }
 
-    public void Teleport(Transform Positionement)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        transform.position = new Vector2(Positionement.position.x, Positionement.position.y);
-    }
+        if (collision.CompareTag("Respawn"))
+        {
+            transform.position = new Vector2(CheckpointPos.x, CheckpointPos.y);
+        }
 
-    public void Reset()
-    {
-        transform.position = new Vector2(CheckpointPos.x, CheckpointPos.y);
+        if (collision.CompareTag("Key"))
+        {
+            door.Open();
+        }
+
+        if (collision.CompareTag("Checkpoint"))
+        {
+            CheckpointPos = transform.position;
+        }
     }
 }
